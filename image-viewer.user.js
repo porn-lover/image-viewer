a82e5a6300615120e66676b29a735e4d9448f4e9
porn-lover <rpva81+anonymus@gmail.com>
2025-05-31 23:23:34 +0200

    add postimg.cc

--- image-viewer.user.js
+++ image-viewer.user.js
@@ -2,12 +2,13 @@
 // @name         Image viewer
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
 // @namespace    http://tampermonkey.net/
-// @version      2025-05-31
+// @version      2025-06-01
 // @description  skip the hassle
 // @author       porn-lover
 // @match        https://fastpic.org/view/*
 // @match        https://pixhost.to/show/*
 // @match        https://imgbox.com/*
+// @match        https://postimg.cc/*
 // ==/UserScript==
 
 (function() {
@@ -17,6 +18,7 @@
         'fastpic.org': {element: '.img-a img', attr: 'src'},
         'pixhost.to': {element: 'img#image', attr: 'src'},
         'imgbox.com': {element: 'img#img', attr: 'src'},
+        'postimg.cc': {element: 'img#main-image', attr: 'src'},
     };
 
     const intervalTime = 100; // 100 milliseconden
