98e961ce2474e62f5c99a945d1874808cf725d4a
porn-lover <rpva81+anonymus@gmail.com>
2025-06-04 23:07:29 +0200

    faster response, alfabetical hosts, add imagebam

--- image-viewer.user.js
+++ image-viewer.user.js
@@ -2,12 +2,13 @@
 // @name         Image viewer
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
 // @namespace    http://tampermonkey.net/
-// @version      2025-06-01
+// @version      2025-06-04
 // @description  skip the hassle
 // @author       porn-lover
 // @match        https://fastpic.org/view/*
-// @match        https://pixhost.to/show/*
 // @match        https://imgbox.com/*
+// @match        https://imagebam.com/view/*
+// @match        https://pixhost.to/show/*
 // @match        https://postimg.cc/*
 // ==/UserScript==
 
@@ -16,12 +17,13 @@
 
     var urls = {
         'fastpic.org': {element: '.img-a img', attr: 'src'},
-        'pixhost.to': {element: 'img#image', attr: 'src'},
+        'imagebam.com': {element: 'img.main-image', attr: 'src'},
         'imgbox.com': {element: 'img#img', attr: 'src'},
+        'pixhost.to': {element: 'img#image', attr: 'src'},
         'postimg.cc': {element: 'img#main-image', attr: 'src'},
     };
 
-    const intervalTime = 100; // 100 milliseconden
+    const intervalTime = 50; // 100 milliseconden
     const stopTime = 5000;    // 5 seconden
     const obj = urls[window.location.hostname];
 
