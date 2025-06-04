117b87b1d426717e907b5c408e4b712a9d1241fd
porn-lover <rpva81+anonymus@gmail.com>
2025-06-04 23:53:44 +0200

    Update image-viewer.user.js

--- image-viewer.user.js
+++ image-viewer.user.js
@@ -2,7 +2,7 @@
 // @name         Image viewer
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
 // @namespace    http://tampermonkey.net/
-// @version      2025-06-04f
+// @version      2025-06-04g
 // @description  skip the hassle
 // @author       porn-lover
 // @match        *://*.fastpic.org/view/*
@@ -17,7 +17,7 @@
 
     var urls = {
         'fastpic.org': {element: '.img-a img', attr: 'src'},
-        'imagebam.com': {element: 'img.main-image', attr: 'src', continue: {element: '[data-shown="inter"]', action: "$('[data-shown=\"inter\"]').click();window.location = window.location.href;"}},
+        'imagebam.com': {element: 'img.main-image', attr: 'src', continue: {element: '[data-shown="inter"]', action: "var date = new Date();date.setTime(date.getTime() + (6 * 60 * 60 * 1000));expires = \"; expires=\" + date.toUTCString();document.cookie = \"nsfw_inter=1\" + expires + \"; path=/\";window.location.reload();"}},
         'imgbox.com': {element: 'img#img', attr: 'src'},
         'pixhost.to': {element: 'img#image', attr: 'src'},
         'postimg.cc': {element: 'img#main-image', attr: 'src'},
